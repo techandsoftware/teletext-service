466bbea237eec9eb7facbfa38f773f51fa478235
Rob H <techandsoftware_commits@outlook.com>
2025-11-22 19:20:50 +0000

    vite builds in app and library mode

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: © 2025 Rob Hardy
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// issue - vite doesn't minify es libs https://github.com/vitejs/vite/issues/6555 so we minify after vite in the build script
+
+const BANNER = `/*! @techandsoftware/teletext-service
+    SPDX${''}-FileCopyrightText: (c) ${new Date().getUTCFullYear()} Rob Hardy
+    SPDX${''}-License-Identifier: AGPL-3.0-only */`;
+
+export default defineConfig({
+  build: {
+    sourcemap: true,
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        banner: BANNER,
+      }
+    }
+  }
+});
---
+++ vite.lib.config.js
@@ -0,0 +1,33 @@
+// SPDX-FileCopyrightText: © 2025 Rob Hardy
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// issue - vite doesn't minify es libs https://github.com/vitejs/vite/issues/6555 so we minify after vite in the build script
+
+const BANNER = `/*! @techandsoftware/teletext-service
+    SPDX${''}-FileCopyrightText: (c) ${new Date().getUTCFullYear()} Rob Hardy
+    SPDX${''}-License-Identifier: AGPL-3.0-only */`;
+
+export default defineConfig({
+  build: {
+    sourcemap: true,
+    outDir: 'libdist',
+    emptyOutDir: false,
+    lib: {
+      entry: resolve(__dirname, 'lib/ModuleExports.js'),
+      name: '@techandsoftware/teletext-service',
+      fileName: 'teletext-service',
+      formats: ['es']
+    },
+    rollupOptions: {
+      output: {
+        banner: BANNER,
+      }
+    }
+  }
+});
